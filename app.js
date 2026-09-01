80e1b6a05634013c18fca2c251f51ecc5dbfea13
prince11724 <prince1108ch@gmail.com>
2026-09-01 21:38:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ async function handleLogin(event) {
 
         const response =
             await fetch(
-                "http://127.0.0.1:5000/api/login",
+                `${API_BASE_URL}/login`,
                 {
 
                     method: "POST",
